904092ee79b4850b2153780141d821cb0a0e299f
V1ct0rMP <111666003+V1ct0rMP@users.noreply.github.com>
2022-09-04 13:30:22 -0500

    Create Cuadrado.py

---
+++ Dia26Agosto2022/operaciones/Cuadrado.py
@@ -0,0 +1,3 @@
+def cuadrado(a:int|float)->int|float: ##Se crea una funcion que pide como parametro un dato ya sea de tipo integer(int) o flotante(float) y su salida sera un valor ya sea tipo int o float
+    return a*a #La funcion devuelve el resultado de multiplicar el dato por si mismo, es decir elevar al cuadrado
+#Hecho por: Victor Jesus Martinez Perez
