b2db70a5ac7e1fd6e5c055693bb2594f4a66016e
V1ct0rMP <111666003+V1ct0rMP@users.noreply.github.com>
2022-09-04 13:16:09 -0500

    Update Suma.py

--- Dia26Agosto2022/operaciones/Suma.py
+++ Dia26Agosto2022/operaciones/Suma.py
@@ -1,3 +1,3 @@
-def sumar(a:int|float, b:int|float): #Se crea una funcion que pide como parametro un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+def sumar(a:int|float, b:int|float) -> int|float: #Se crea una funcion que pide como parametro un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
     return a + b #Se retorna el resultado de la suma entre los dos datos
 #Hecho por: Victor Jesus Martinez Perez
