e6b7ce20fcaa2b9086092d7b8b1ae96fafbf1db4
V1ct0rMP <111666003+V1ct0rMP@users.noreply.github.com>
2022-09-04 13:20:03 -0500

    Create Multiplicar.py

---
+++ Dia26Agosto2022/operaciones/Multiplicar.py
@@ -0,0 +1,3 @@
+def multiplicar(a:int|float,b:int|float)->int|float: #Se crea una funcion que pide como parametro un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+    return a*b #La funcion devuelve el resultado de la multiplicacion entre los dos datos
+#Hecho por: Victor Jesus Martinez Perez
