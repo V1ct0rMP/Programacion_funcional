5e399c31f78856b255bf5f2e1b0883c83d0704d2
V1ct0rMP <111666003+V1ct0rMP@users.noreply.github.com>
2022-09-04 13:35:05 -0500

    Create Operaciones.py

---
+++ Dia26Agosto2022/operaciones/Operaciones.py
@@ -0,0 +1,15 @@
+#Se Juntan en un mismo archivos las operaciones anteriores
+def sumar(a:int|float, b:int|float)->int|float: #Se crea una funcion que pide como parametros un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+    return a + b #Se retorna el resultado de la suma entre los dos datos
+
+def restar(a:int|float, b:int|float)->int|float: #Se crea una funcion que pide como parametros un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+    return a- b #Se retorna el resultado de la resta entre los dos datos
+
+def multiplicar(a:int|float,b:int|float)->int|float: #Se crea una funcion que pide como parametros un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+    return a*b #Se retorna el resultado de la multiplicacion entre los dos datos
+
+def dividir(a:int|float, b:int|float)->int|float: #Se crea una funcion que pide como parametros un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+    return a/b #Se retorna el resultado de la division entre los dos datos
+
+def cuadrado(a:int|float)->int|float: #Se crea una funcion que pide como parametro un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+    return a*a #Se retorna el resultado de multiplicar el dato por si mimso o mejor dicho elevar al cuadrado
