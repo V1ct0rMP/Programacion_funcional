f217907e2dd52902d7ecd5514a140afb0fa6dbdf
V1ct0rMP <111666003+V1ct0rMP@users.noreply.github.com>
2022-09-04 13:17:53 -0500

    Update Resta.py

--- Dia26Agosto2022/operaciones/Resta.py
+++ Dia26Agosto2022/operaciones/Resta.py
@@ -1,2 +1,3 @@
-def restar(a:int|float, b:int|float):
-    return a-b
+def restar(a:int|float, b:int|float)->int|float: #Se crea una funcion que pide como parametro un dato ya sea de tipo integer(int) o flotante(float) y retorna un valor ya sea tipo int o float
+    return a-b #Se devuelve el resultado de la resta de los dos datos
+#Hecho por: Victor Jesus Martinez Perez
