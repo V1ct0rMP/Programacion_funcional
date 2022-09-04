c707ba2849935520407474aab5a991ba855cd7e3
V1ct0rMP <111666003+V1ct0rMP@users.noreply.github.com>
2022-09-04 13:23:02 -0500

    Create Dividir.py

---
+++ Dia26Agosto2022/operaciones/Dividir.py
@@ -0,0 +1,3 @@
+def dividir(a:int|float, b:int|float)->int|float: #Se crea una funcion que pide como parametro un dato ya sea de tipo integer(int) o flotante(float) y su salida sera un valor ya sea tipo int o float
+    return a/b #La funcion devuelve el resultado de la division entre los dos datos
+#Hecho por: Victor Jesus Martinez Perez
